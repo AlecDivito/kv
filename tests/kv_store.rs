3db31573e9f66dbde28855d36396da7758171abe
Alec Di Vito <alec.divito@gmail.com>
2021-09-07 23:01:44 -0400

    fixed tests

--- tests/kv_store.rs
+++ tests/kv_store.rs
@@ -5,88 +5,112 @@ use tempfile::TempDir;
 use walkdir::WalkDir;
 
 // Should get previously stored value
-#[test]
-fn get_stored_value() -> Result<()> {
+#[tokio::test]
+async fn get_stored_value() -> Result<()> {
     let temp_dir = TempDir::new().expect("unable to create temporary working directory");
-    let store = KvStore::open(temp_dir.path())?;
+    let store = KvStore::open(temp_dir.path().to_path_buf()).await?;
 
-    store.set("key1".to_owned(), "value1".to_owned())?;
-    store.set("key2".to_owned(), "value2".to_owned())?;
+    store.set("key1".to_owned(), "value1".to_owned()).await?;
+    store.set("key2".to_owned(), "value2".to_owned()).await?;
 
-    assert_eq!(store.get("key1".to_owned())?, Some("value1".to_owned()));
-    assert_eq!(store.get("key2".to_owned())?, Some("value2".to_owned()));
+    assert_eq!(
+        store.get("key1".to_owned()).await?,
+        Some("value1".to_owned())
+    );
+    assert_eq!(
+        store.get("key2".to_owned()).await?,
+        Some("value2".to_owned())
+    );
 
     // Open from disk again and check persistent data
     drop(store);
-    let store = KvStore::open(temp_dir.path())?;
-    assert_eq!(store.get("key1".to_owned())?, Some("value1".to_owned()));
-    assert_eq!(store.get("key2".to_owned())?, Some("value2".to_owned()));
+    let store = KvStore::open(temp_dir.into_path()).await?;
+    assert_eq!(
+        store.get("key1".to_owned()).await?,
+        Some("value1".to_owned())
+    );
+    assert_eq!(
+        store.get("key2".to_owned()).await?,
+        Some("value2".to_owned())
+    );
 
     Ok(())
 }
 
 // Should overwrite existent value
-#[test]
-fn overwrite_value() -> Result<()> {
+#[tokio::test]
+async fn overwrite_value() -> Result<()> {
     let temp_dir = TempDir::new().expect("unable to create temporary working directory");
-    let store = KvStore::open(temp_dir.path())?;
+    let store = KvStore::open(temp_dir.path().to_path_buf()).await?;
 
-    store.set("key1".to_owned(), "value1".to_owned())?;
-    assert_eq!(store.get("key1".to_owned())?, Some("value1".to_owned()));
-    store.set("key1".to_owned(), "value2".to_owned())?;
-    assert_eq!(store.get("key1".to_owned())?, Some("value2".to_owned()));
+    store.set("key1".to_owned(), "value1".to_owned()).await?;
+    assert_eq!(
+        store.get("key1".to_owned()).await?,
+        Some("value1".to_owned())
+    );
+    store.set("key1".to_owned(), "value2".to_owned()).await?;
+    assert_eq!(
+        store.get("key1".to_owned()).await?,
+        Some("value2".to_owned())
+    );
 
     // Open from disk again and check persistent data
     drop(store);
-    let store = KvStore::open(temp_dir.path())?;
-    assert_eq!(store.get("key1".to_owned())?, Some("value2".to_owned()));
-    store.set("key1".to_owned(), "value3".to_owned())?;
-    assert_eq!(store.get("key1".to_owned())?, Some("value3".to_owned()));
+    let store = KvStore::open(temp_dir.into_path()).await?;
+    assert_eq!(
+        store.get("key1".to_owned()).await?,
+        Some("value2".to_owned())
+    );
+    store.set("key1".to_owned(), "value3".to_owned()).await?;
+    assert_eq!(
+        store.get("key1".to_owned()).await?,
+        Some("value3".to_owned())
+    );
 
     Ok(())
 }
 
 // Should get `None` when getting a non-existent key
-#[test]
-fn get_non_existent_value() -> Result<()> {
+#[tokio::test]
+async fn get_non_existent_value() -> Result<()> {
     let temp_dir = TempDir::new().expect("unable to create temporary working directory");
-    let store = KvStore::open(temp_dir.path())?;
+    let store = KvStore::open(temp_dir.path().to_path_buf()).await?;
 
-    store.set("key1".to_owned(), "value1".to_owned())?;
-    assert_eq!(store.get("key2".to_owned())?, None);
+    store.set("key1".to_owned(), "value1".to_owned()).await?;
+    assert_eq!(store.get("key2".to_owned()).await?, None);
 
     // Open from disk again and check persistent data
     drop(store);
-    let store = KvStore::open(temp_dir.path())?;
-    assert_eq!(store.get("key2".to_owned())?, None);
+    let store = KvStore::open(temp_dir.into_path()).await?;
+    assert_eq!(store.get("key2".to_owned()).await?, None);
 
     Ok(())
 }
 
-#[test]
-fn remove_non_existent_key() -> Result<()> {
+#[tokio::test]
+async fn remove_non_existent_key() -> Result<()> {
     let temp_dir = TempDir::new().expect("unable to create temporary working directory");
-    let store = KvStore::open(temp_dir.path())?;
-    assert!(store.remove("key1".to_owned()).is_err());
+    let store = KvStore::open(temp_dir.into_path()).await?;
+    assert!(store.remove("key1".to_owned()).await.is_err());
     Ok(())
 }
 
-#[test]
-fn remove_key() -> Result<()> {
+#[tokio::test]
+async fn remove_key() -> Result<()> {
     let temp_dir = TempDir::new().expect("unable to create temporary working directory");
-    let store = KvStore::open(temp_dir.path())?;
-    store.set("key1".to_owned(), "value1".to_owned())?;
-    assert!(store.remove("key1".to_owned()).is_ok());
-    assert_eq!(store.get("key1".to_owned())?, None);
+    let store = KvStore::open(temp_dir.into_path()).await?;
+    store.set("key1".to_owned(), "value1".to_owned()).await?;
+    assert!(store.remove("key1".to_owned()).await.is_ok());
+    assert_eq!(store.get("key1".to_owned()).await?, None);
     Ok(())
 }
 
 // Insert data until total size of the directory decreases.
 // Test data correctness after compaction.
-#[test]
-fn compaction() -> Result<()> {
+#[tokio::test]
+async fn compaction() -> Result<()> {
     let temp_dir = TempDir::new().expect("unable to create temporary working directory");
-    let store = KvStore::open(temp_dir.path())?;
+    let store = KvStore::open(temp_dir.path().to_path_buf()).await?;
 
     let dir_size = || {
         let entries = WalkDir::new(temp_dir.path()).into_iter();
@@ -104,7 +128,7 @@ fn compaction() -> Result<()> {
         for key_id in 0..1000 {
             let key = format!("key{}", key_id);
             let value = format!("{}", iter);
-            store.set(key, value)?;
+            store.set(key, value).await?;
         }
 
         let new_size = dir_size();
@@ -116,10 +140,10 @@ fn compaction() -> Result<()> {
 
         drop(store);
         // reopen and check content
-        let store = KvStore::open(temp_dir.path())?;
+        let store = KvStore::open(temp_dir.into_path()).await?;
         for key_id in 0..1000 {
             let key = format!("key{}", key_id);
-            assert_eq!(store.get(key)?, Some(format!("{}", iter)));
+            assert_eq!(store.get(key).await?, Some(format!("{}", iter)));
         }
         return Ok(());
     }
@@ -127,17 +151,18 @@ fn compaction() -> Result<()> {
     panic!("No compaction detected");
 }
 
-#[test]
-fn concurrent_set() -> Result<()> {
+#[tokio::test]
+async fn concurrent_set() -> Result<()> {
     let temp_dir = TempDir::new().expect("unable to create temporary working directory");
-    let store = KvStore::open(temp_dir.path())?;
+    let store = KvStore::open(temp_dir.path().to_path_buf()).await?;
     let barrier = Arc::new(Barrier::new(1001));
     for i in 0..1000 {
         let store = store.clone();
         let barrier = barrier.clone();
-        thread::spawn(move || {
+        tokio::spawn(async move {
             store
                 .set(format!("key{}", i), format!("value{}", i))
+                .await
                 .unwrap();
             barrier.wait();
         });
@@ -145,37 +170,44 @@ fn concurrent_set() -> Result<()> {
     barrier.wait();
 
     for i in 0..1000 {
-        assert_eq!(store.get(format!("key{}", i))?, Some(format!("value{}", i)));
+        assert_eq!(
+            store.get(format!("key{}", i)).await?,
+            Some(format!("value{}", i))
+        );
     }
 
     // Open from disk again and check persistent data
     drop(store);
-    let store = KvStore::open(temp_dir.path())?;
+    let store = KvStore::open(temp_dir.into_path()).await?;
     for i in 0..1000 {
-        assert_eq!(store.get(format!("key{}", i))?, Some(format!("value{}", i)));
+        assert_eq!(
+            store.get(format!("key{}", i)).await?,
+            Some(format!("value{}", i))
+        );
     }
 
     Ok(())
 }
 
-#[test]
-fn concurrent_get() -> Result<()> {
+#[tokio::test]
+async fn concurrent_get() -> Result<()> {
     let temp_dir = TempDir::new().expect("unable to create temporary working directory");
-    let store = KvStore::open(temp_dir.path())?;
+    let store = KvStore::open(temp_dir.path().to_path_buf()).await?;
     for i in 0..100 {
         store
             .set(format!("key{}", i), format!("value{}", i))
+            .await
             .unwrap();
     }
 
     let mut handles = Vec::new();
     for thread_id in 0..100 {
         let store = store.clone();
-        let handle = thread::spawn(move || {
+        let handle = tokio::spawn(async move {
             for i in 0..100 {
                 let key_id = (i + thread_id) % 100;
                 assert_eq!(
-                    store.get(format!("key{}", key_id)).unwrap(),
+                    store.get(format!("key{}", key_id)).await.unwrap(),
                     Some(format!("value{}", key_id))
                 );
             }
@@ -183,20 +215,20 @@ fn concurrent_get() -> Result<()> {
         handles.push(handle);
     }
     for handle in handles {
-        handle.join().unwrap();
+        handle.await.unwrap();
     }
 
     // Open from disk again and check persistent data
     drop(store);
-    let store = KvStore::open(temp_dir.path())?;
+    let store = KvStore::open(temp_dir.into_path()).await?;
     let mut handles = Vec::new();
     for thread_id in 0..100 {
         let store = store.clone();
-        let handle = thread::spawn(move || {
+        let handle = tokio::spawn(async move {
             for i in 0..100 {
                 let key_id = (i + thread_id) % 100;
                 assert_eq!(
-                    store.get(format!("key{}", key_id)).unwrap(),
+                    store.get(format!("key{}", key_id)).await.unwrap(),
                     Some(format!("value{}", key_id))
                 );
             }
@@ -204,7 +236,7 @@ fn concurrent_get() -> Result<()> {
         handles.push(handle);
     }
     for handle in handles {
-        handle.join().unwrap();
+        handle.await.unwrap();
     }
 
     Ok(())
